0ab3c7fde1ad16adf4dc1447f0586176beefcae3
Eliezer Abate <code.eliezer@gmail.com>
2017-10-22 10:07:18 -0700

    Initial Commit

---
+++ weekly-summary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weekly-summary
+//
+//  Created by Eliezer Abate on 10/22/17.
+//  Copyright © 2017 Eliezer Abate. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
